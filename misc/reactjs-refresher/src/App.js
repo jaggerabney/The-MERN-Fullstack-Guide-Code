d4cf410f3408f624914e50437eaa4810891fb63b
jaggerabney <jagger.abney@gmail.com>
2023-04-18 17:59:29 -0700

    moved goals into state

--- misc/reactjs-refresher/src/App.js
+++ misc/reactjs-refresher/src/App.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GoalList from "./components/GoalList/GoalList";
 import NewGoal from "./components/NewGoal/NewGoal";
 
 import "./App.css";
 
+const DUMMY_GOALS = [
+  { id: "cg1", text: "Finish the course!" },
+  { id: "cg2", text: "Learn all about course topics" },
+  { id: "cg3", text: "Help other students in the course Q&A" },
+];
+
 const App = () => {
-  const DUMMY_GOALS = [
-    { id: "cg1", text: "Finish the course!" },
-    { id: "cg2", text: "Learn all about course topics" },
-    { id: "cg3", text: "Help other students in the course Q&A" },
-  ];
+  const [goals, setGoals] = useState(DUMMY_GOALS);
 
   function newGoalHandler(newGoal) {
-    DUMMY_GOALS.push(newGoal);
-
-    console.log(DUMMY_GOALS);
+    setGoals(goals.concat(newGoal));
   }
 
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
       <NewGoal onCreate={newGoalHandler} />
-      <GoalList goals={DUMMY_GOALS} />
+      <GoalList goals={goals} />
     </div>
   );
 };
