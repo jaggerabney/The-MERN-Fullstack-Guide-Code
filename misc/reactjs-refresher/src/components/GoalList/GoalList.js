c0c9d05ea0f24af8b57c626f84e3c5b48c3ae0e5
jaggerabney <jagger.abney@gmail.com>
2023-04-18 17:40:53 -0700

    used map function to render list dynamically

--- misc/reactjs-refresher/src/components/GoalList/GoalList.js
+++ misc/reactjs-refresher/src/components/GoalList/GoalList.js
@@ -3,9 +3,13 @@ import React from "react";
 import "./GoalList.css";
 
 function GoalList(props) {
-  console.log(props.goals);
-
-  return <ul className="goal-list"></ul>;
+  return (
+    <ul className="goal-list">
+      {props.goals.map((goal) => (
+        <li key={goal.id}>{goal.text}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default GoalList;
