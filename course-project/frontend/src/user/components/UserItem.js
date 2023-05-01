f3dc67837c9415067e0f2406519238c0f38d72d6
jaggerabney <jagger.abney@gmail.com>
2023-04-30 18:51:44 -0700

    implemented UserItem component

--- course-project/frontend/src/user/components/UserItem.js
+++ course-project/frontend/src/user/components/UserItem.js
@@ -2,6 +2,22 @@ import React from "react";
 
 import classes from "./UserItem.module.css";
 
-function UserItem(props) {}
+function UserItem(props) {
+  return (
+    <li className={classes["user-item"]}>
+      <div className={classes["user-item__content"]}>
+        <div className={classes["user-item__image"]}>
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className={classes["user-item__info"]}>
+          <h2>{props.name}</h2>
+          <h3>
+            {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
+}
 
 export default UserItem;
