5dd39316b1a3a33e5f68855d70e5d38d5dc1dfb8
jaggerabney <jagger.abney@gmail.com>
2023-04-30 18:38:08 -0700

    defined frontend structure, Users/NewPlace pages

--- course-project/frontend/src/App.js
+++ course-project/frontend/src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
