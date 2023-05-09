d25e07972e1444d71588a839ecba77e8a7d8b783
jaggerabney <jagger.abney@gmail.com>
2023-05-08 21:19:46 -0700

    explored sending requests and responses with node

--- misc/nodejs-refresher/app.js
+++ misc/nodejs-refresher/app.js
@@ -3,7 +3,21 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(`Incoming request of type ${req.method} from ${req.url}`);
 
-  res.end("<h1>Success!</h1>");
+  if (req.method === "POST") {
+    let body = "";
+
+    req.on("end", () => {
+      const username = body.split("=")[1];
+
+      res.end(`<h1>${username}</h1>`);
+    });
+    req.on("data", (chunk) => (body += chunk));
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      '<form method="POST"><input type="text" name="username"><button type="submit">Create user</button></form>'
+    );
+  }
 });
 
 server.listen(5000);
