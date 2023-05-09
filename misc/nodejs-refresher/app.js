3bac1bed8e077a5b5dbe2e3424711cc0cadd3e3a
jaggerabney <jagger.abney@gmail.com>
2023-05-08 21:34:55 -0700

    explored sending requests/responses with express

--- misc/nodejs-refresher/app.js
+++ misc/nodejs-refresher/app.js
@@ -1,23 +1,33 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  console.log(`Incoming request of type ${req.method} from ${req.url}`);
+const app = express();
 
-  if (req.method === "POST") {
-    let body = "";
+app.use((req, res, next) => {
+  let body = "";
 
-    req.on("end", () => {
-      const username = body.split("=")[1];
+  req.on("end", () => {
+    const username = body.split("=")[1];
 
-      res.end(`<h1>${username}</h1>`);
-    });
-    req.on("data", (chunk) => (body += chunk));
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      '<form method="POST"><input type="text" name="username"><button type="submit">Create user</button></form>'
-    );
-  }
+    if (username) {
+      req.body = { name: username };
+    }
+
+    next();
+  });
+
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
 });
 
-server.listen(5000);
+app.use((req, res, next) => {
+  if (req.body) {
+    return res.send(`<h1>${req.body.name}</h1>`);
+  }
+
+  res.send(
+    '<form method="POST"><input type="text" name="username"><button type="submit">Create user</buton></form>'
+  );
+});
+
+app.listen(5000);
