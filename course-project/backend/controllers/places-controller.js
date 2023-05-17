611dd39ecc9f935250275cd1a1a248175a9ac866
jaggerabney <jagger.abney@gmail.com>
2023-05-16 21:23:06 -0700

    exported logic from routes to a controller

---
+++ course-project/backend/controllers/places-controller.js
@@ -0,0 +1,42 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.74861523068957,
+      lng: -73.98524597764153,
+    },
+    creator: "u1",
+  },
+];
+
+function getPlaceById(req, res, next) {
+  const { placeId } = req.params;
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  if (!place) {
+    throw new HttpError("Couldn't find a place for that ID!", 404);
+  }
+
+  res.json({ place });
+}
+
+function getPlacesByUserId(req, res, next) {
+  const { userId } = req.params;
+  const places = DUMMY_PLACES.filter((place) => place.creator === userId);
+
+  if (places.length < 1) {
+    throw new HttpError("Couldn't find places for the given user ID!", 404);
+  }
+
+  res.json({ places });
+}
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
--- course-project/backend/routes/places-routes.js
+++ course-project/backend/routes/places-routes.js
@@ -1,45 +1,10 @@
 const express = require("express");
 
+const placesController = require("../controllers/places-controller");
+
 const router = express.Router();
 
-const HttpError = require("../models/http-error");
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.74861523068957,
-      lng: -73.98524597764153,
-    },
-    creator: "u1",
-  },
-];
-
-router.get("/:placeId", (req, res, next) => {
-  const { placeId } = req.params;
-  const place = DUMMY_PLACES.find((place) => place.id === placeId);
-
-  if (!place) {
-    throw new HttpError("Couldn't find a place for that ID!", 404);
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:userId", (req, res, next) => {
-  const { userId } = req.params;
-  const places = DUMMY_PLACES.filter((place) => place.creator === userId);
-
-  if (places.length < 1) {
-    throw new HttpError("Couldn't find palces for the given user ID!", 404);
-  }
-
-  res.json({ places });
-});
+router.get("/:placeId", placesController.getPlaceById);
+router.get("/user/:userId", placesController.getPlacesByUserId);
 
 module.exports = router;
