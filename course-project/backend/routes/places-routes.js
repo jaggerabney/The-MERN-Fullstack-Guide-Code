04bc508db992dbb624f064aaf6c2bc4a7d71ad5d
jaggerabney <jagger.abney@gmail.com>
2023-05-16 20:52:59 -0700

    added actual GET route for "/:placeId"

--- course-project/backend/routes/places-routes.js
+++ course-project/backend/routes/places-routes.js
@@ -2,10 +2,27 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET request in places");
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.74861523068957,
+      lng: -73.98524597764153,
+    },
+    creator: "u1",
+  },
+];
 
-  res.json({ message: "Success!" });
+router.get("/:placeId", (req, res, next) => {
+  const placeId = req.params.placeId;
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  res.json({ place });
 });
 
 module.exports = router;
