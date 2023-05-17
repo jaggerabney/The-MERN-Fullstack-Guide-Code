e683d06e65d43b6f57ef675cbe4b5cdcec4dbc75
jaggerabney <jagger.abney@gmail.com>
2023-05-17 14:44:59 -0700

    added POST route for "/api/users"

--- course-project/backend/controllers/users-controller.js
+++ course-project/backend/controllers/users-controller.js
@@ -1,5 +1,28 @@
+let dummyUsers = [
+  {
+    name: "Jagger",
+    email: "test@test.com",
+    password: "password",
+  },
+  {
+    name: "Kira",
+    email: "test2@test.com",
+    password: "password2",
+  },
+];
+
 function getUsers(req, res, next) {
   res.status(200).json({ users: dummyUsers });
 }
 
+function addUser(req, res, next) {
+  const { name, email, password } = req.body;
+  const newUser = { name, email, password };
+
+  dummyUsers.push(newUser);
+
+  res.status(201).json({ user: newUser });
+}
+
 exports.getUsers = getUsers;
+exports.addUser = addUser;
--- course-project/backend/routes/users-routes.js
+++ course-project/backend/routes/users-routes.js
@@ -2,21 +2,9 @@ const express = require("express");
 
 const usersController = require("../controllers/users-controller");
 
-let dummyUsers = [
-  {
-    name: "Jagger",
-    email: "test@test.com",
-    password: "password",
-  },
-  {
-    name: "Kira",
-    email: "test2@test.com",
-    password: "password2",
-  },
-];
-
 const router = express.Router();
 
 router.get("/", usersController.getUsers);
+router.post("/", usersController.addUser);
 
 module.exports = router;
